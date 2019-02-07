e4f9dd779f5390fcfa82b5ea5108a32210ae18bf
AMAR PATEL <32103452+AMARPATEEL@users.noreply.github.com>
2019-02-07 20:25:05 +0530

    Add files via upload

---
+++ selection sort.py
@@ -0,0 +1,13 @@
+def selectionSort(alist):
+   for fillslot in range(len(alist)-1,0,-1):
+       positionOfMax=0
+       for location in range(1,fillslot+1):
+           if alist[location]>alist[positionOfMax]:
+               positionOfMax = location
+
+       temp = alist[fillslot]
+       alist[fillslot] = alist[positionOfMax]
+       alist[positionOfMax] = temp
+alist=[54,45,67,12,34,98,66]
+selectionSort(alist)
+print(alist)
